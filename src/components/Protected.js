604649d54a9b90eb490acf41028b000401e6211a
ahmar7 <ahmarjabbar7@gmail.com>
2023-10-08 19:19:37 +0500

    update

--- src/components/Protected.js
+++ src/components/Protected.js
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { getAuthApi } from "../Api/service";
 import { useNavigate } from "react-router-dom";
-
 const Protected = ({ Component }) => {
   let Navigate = useNavigate();
   const [isLoading, setisLoading] = useState(true);
   const getAdminAuth = async () => {
     try {
       const updateAbout = await getAuthApi();
+      console.log(updateAbout);
       if (updateAbout.success === false) {
         Navigate("/admin-login");
 
+        return;
+      } else if (updateAbout.status === 401 || 403) {
+        Navigate("/admin-login");
         return;
       }
     } catch (error) {
