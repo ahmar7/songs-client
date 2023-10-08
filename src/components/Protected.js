f2d07f762087e7a057d8cbb29636676f2d301f8a
ahmar7 <ahmarjabbar7@gmail.com>
2023-10-08 19:25:53 +0500

    update

--- src/components/Protected.js
+++ src/components/Protected.js
@@ -4,24 +4,46 @@ import { useNavigate } from "react-router-dom";
 const Protected = ({ Component }) => {
   let Navigate = useNavigate();
   const [isLoading, setisLoading] = useState(true);
-  const getAdminAuth = async () => {
+  let getAdminAuth = async () => {
+    setisDisable(true);
     try {
-      const updateAbout = await getAuthApi();
-      console.log(updateAbout);
-      if (updateAbout.success === false) {
-        Navigate("/admin-login");
+      let res = await fetch(`${baseUrl}adminAuth`, {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+        credentials: "include",
+      });
 
+      if (res.status === 401 || 403) {
+        Navigate("/admin-login");
         return;
-      } else if (updateAbout.status === 401 || 403) {
+      } else if (!res) {
         Navigate("/admin-login");
         return;
       }
+      setisLoading(false);
     } catch (error) {
-      alert(error?.data?.msg || error?.message || "Something went wrong");
-    } finally {
       setisLoading(false);
     }
   };
+
+  //   const getAdminAuth = async () => {
+  //     try {
+  //       const updateAbout = await getAuthApi();
+  //       console.log(updateAbout);
+  //       if (updateAbout.success === false) {
+  //         Navigate("/admin-login");
+
+  //         return;
+  //       } else if (updateAbout.status === 401 || 403) {
+  //         Navigate("/admin-login");
+  //         return;
+  //       }
+  //     } catch (error) {
+  //       alert(error?.data?.msg || error?.message || "Something went wrong");
+  //     } finally {
+  //       setisLoading(false);
+  //     }
+  //   };
   useEffect(() => {
     getAdminAuth();
   });
